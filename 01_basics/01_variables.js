c04a7bf9cfba9b7f9cb6d17c97224dadc0733c9e
Pankajd3699 <85942284+Pankajd3699@users.noreply.github.com>
2024-06-21 13:19:52 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 14453
+let accountEmail = "pankubhaiya36@gmailcom"
+var accountPass = "Sd@030699"
+acountCity = "guna"
+let accountState;
+
+//accountID = 1
+accountEmail = "newEmail@gmail.com"
+accountPass = "12345"
+acountCity = "indore"
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPass, acountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as a new version
+
+//alert("hello") //w eare using node note browser
+
+console.log(3+3)
+console.log("pankaj")
+
+
+let name = "pankaj"
+let age = 25
+let weight = 69.00
+let IsLoggenIn = false
+
+
+// number ==> 2 to the power 53
+// string ==> ""
+// bnoolean ==> true or false
+// null ==> standalone 
+// undefined ==>
+// symbol ==> to find uniqeness
+
+
+
+// oblect
+
+console.log(typeof undefined);      //undefined 
+console.log(typeof null);           //oblect
